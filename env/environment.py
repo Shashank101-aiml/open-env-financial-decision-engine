86f1222b803c4e1f73a31d6cedb5931cc79cb969
Agrawal-Shashank <shag23aiml@cmrit.ac.in>
2026-04-08 17:39:04 +0530

    Update environment.py

--- env/environment.py
+++ env/environment.py
@@ -1,1 +1,29 @@
+from env.models import Observation, Action, Reward
+from tasks.loan_task import get_loan_task
+from graders.loan_grader import grade_loan
 
+class FinancialEnv:
+    def __init__(self):
+        self.current_task = None
+        self.done = False
+
+    def reset(self):
+        self.current_task = get_loan_task()
+        self.done = False
+        return Observation(input_text=self.current_task["input"])
+
+    def step(self, action: Action):
+        score = grade_loan(action, self.current_task)
+
+        reward = Reward(score=score)
+        self.done = True
+
+        return (
+            Observation(input_text="Task completed"),
+            reward,
+            self.done,
+            {}
+        )
+
+    def state(self):
+        return self.current_task
